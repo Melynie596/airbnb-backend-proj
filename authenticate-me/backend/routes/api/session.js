1d850c745d56f755453b0160d209555a10a32bc8
MelynieP <melynie596@gmail.com>
2023-06-06 20:49:46 -0800

    add a backend endpoint to get the current user session

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -53,6 +53,22 @@ router.delete(
   }
 );
 
-//JfzQJ8Qh-hL_pKpgeeXou-UlWVpKE3bYkFdY
+// Restore session user
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
 
 module.exports = router;
