65707719c3e822df1676f6fb40d4b267036d0298
MelynieP <melynie596@gmail.com>
2023-06-19 19:24:31 -0800

    more typo, spot images

--- backend/db/migrations/20230613014813-create-spot-image.js
+++ backend/db/migrations/20230613014813-create-spot-image.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('spotImages', {
+    await queryInterface.createTable('SpotImages', {
       id: {
         allowNull: false,
         autoIncrement: true,
